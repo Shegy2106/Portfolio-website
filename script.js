d6799db422f159ff1b8928e246c4dec9515e956f
Shegy2106 <darko.segvic@gmail.com>
2021-05-18 20:43:52 +0200

    project done
    project done

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+var isAndroid = /android/i.test(navigator.userAgent.toLowerCase());
+var isWindows = /windows phone/i.test(navigator.userAgent.toLowerCase());
+var isBlackberry = /blackberry/i.test(navigator.userAgent.toLowerCase());
+var isiDevice = /ipad|iphone|ipod/i.test(navigator.userAgent.toLowerCase());
 
+if(isAndroid || isWindows || isBlackberry || isiDevice){
+    $(".projects .btn").on('click',function(){
+      alert("Hello! I am an alert box!!");
+         //your code here
+    });
+}else{
+    $(".projects .btn").on('hover',function(){
+      alert("Hello! I am an alert box!!");
+         //your code here
+    });
+}
